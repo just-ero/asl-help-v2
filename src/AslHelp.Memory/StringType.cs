91e99fd45ce4bceda2cdccc49d8315347f3f8e57
Ero <imjustero@gmail.com>
2024-06-13 14:14:30 +0200

    .

--- src/AslHelp.Memory/StringType.cs
+++ src/AslHelp.Memory/StringType.cs
@@ -3,10 +3,13 @@ namespace AslHelp.Memory;
 /// <summary>
 ///     Specifies the kind of string to read from memory.
 /// </summary>
+/// <remarks>
+///     The <see cref="Ipc.IMemoryReader"/> interface uses this enumeration to determine how to read a string from memory.
+/// </remarks>
 public enum StringType
 {
     /// <summary>
-    ///     Automatically detect the string type.
+    ///     Attempt to automatically determine the string type.
     /// </summary>
     Auto,
 
